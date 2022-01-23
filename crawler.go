d7afe756ab360c12467e217eeca29f21d6b2b12d
Arnau Carrasco <arnau.carrasco@gmail.com>
2022-01-23 10:24:17 +0100

    removed cmd args pointers

--- crawler.go
+++ crawler.go
@@ -18,10 +18,10 @@ import (
 )
 
 // Command-Line options
-var verbose *bool      // Verbose bit
-var resume *bool       // Resume last execution
-var initialUrl *string // Initial URL to fetch
-var outputFile *string // Name of the output file
+var verbose bool      // Verbose bit
+var resume bool       // Resume last execution
+var initialUrl string // Initial URL to fetch
+var outputFile string // Name of the output file
 
 // Url type
 type Url struct {
@@ -37,10 +37,10 @@ type Urls struct {
 }
 
 // Variables
-const workingFile string = "working.json"
+const workingFile string = "working.json" // Name of the file where we'll save our progess if the user interrupts the execution
+var urls Urls                             // Main struct for storing urls
 
-var urls Urls // Main struct for storing urls
-
+// Returns a new empty Url object
 func newUrl(u string) *Url {
 	retUrl := Url{URL: u}
 	return &retUrl
@@ -52,44 +52,51 @@ func interruptHandler() {
 	intChan := make(chan os.Signal)
 	signal.Notify(intChan, os.Interrupt, syscall.SIGTERM)
 	go func() {
+		// Wait for os to nofity us using the interrupt channel
 		<-intChan
 		// SIGTERM Received, save our progress and exit!
-
 		log.Println("Program Stopped by user")
 		// Save to disk
 		file, _ := json.MarshalIndent(urls, "", " ")
 		_ = ioutil.WriteFile(workingFile, file, 0644)
-		if *verbose {
+		if verbose {
 			log.Println("Saving working file")
 			log.Printf("Progress saved in working.json (%d urls)\n", len(urls.URLS))
 		}
-
+		// Exit application
 		os.Exit(0)
 	}()
 }
 
+// Mark an URL as completed, and save the error code
 func completeUrl(id int, err int) {
 	logLine(id, "process ended with exit code %d.", err)
 	urls.URLS[id].Completed = true
 	urls.URLS[id].Error = err
 }
+
+// log.Println if verbose bit is set
 func logLine(id int, format string, v ...interface{}) {
-	if *verbose {
+	if verbose {
 		log.Println(fmt.Sprintf(fmt.Sprintf("Process id %d - %s", id, format), v...))
 	}
 }
 
+// Whenever a link is found, we pass the url to this funcion, in order
+// to evalute if we hav to execute a new process walking to it
 func validURL(id int, u string) bool {
+	// Check basic url parsing provided by net/url package
 	_, err := url.ParseRequestURI(u)
 	if err != nil {
 		logLine(id, "url %s is not valid", u)
 		return false
 	}
-	if !strings.HasPrefix(strings.ToLower(u), strings.ToLower(*initialUrl)) {
+	// Check ig the url starts like the initial url
+	if !strings.HasPrefix(strings.ToLower(u), strings.ToLower(initialUrl)) {
 		logLine(id, "url %s is not a child of the initial url", u)
 		return false
 	}
-
+	// Finally, check if the url is not already in our database
 	for url := range urls.URLS {
 		if strings.EqualFold(urls.URLS[url].URL, u) {
 			logLine(id, "url %s already in database", u)
@@ -99,6 +106,7 @@ func validURL(id int, u string) bool {
 	return true
 }
 
+// Returns true if the string "link" exists in the array "links"
 func linkExists(link string, links []string) bool {
 	for index := range links {
 		if strings.EqualFold(link, links[index]) {
@@ -108,66 +116,84 @@ func linkExists(link string, links []string) bool {
 	return false
 }
 
+// This is the funcion we will fire for each url, it will fetch the url and parse the response
 func fetchUrl(id int, u string) {
+	// Check out if we're in resume mode, just for logging purpuses
 	action := "fetching"
-	if *resume {
+	if resume {
 		action = "resuming "
 	}
 	logLine(id, "%s url %s", action, u)
+	// Fetch the url
 	response, err := http.Get(u)
 	if err != nil {
-		completeUrl(id, 1) // Error 1 => Probably network related (dns, unable to connect...)
+		// Error 1 => Probably network related (dns, unable to connect...)
+		completeUrl(id, 1)
 		return
 	}
+	// Error 2 => Response code from http server is not 200
 	if response.StatusCode != 200 {
-		completeUrl(id, 2) // Error 2 => Response code from http server is not 200
+		completeUrl(id, 2)
 		return
 	}
-	// Check Content Type
+	// Error 3 => The document fetched content is not HTML
 	if !strings.HasPrefix(response.Header.Get("Content-Type"), "text/html") {
-		completeUrl(id, 3) // Error 3 => The document fetched content is not HTML
+		completeUrl(id, 3)
 		return
 	}
+	// Error 3 => Error while parsing HTML document
 	body, err := html.Parse(response.Body)
 	response.Body.Close()
 	if err != nil {
-		completeUrl(id, 4) // Error 3 => Unable to parse HTML document
+		completeUrl(id, 4)
 		return
 	}
+	// Extract links from document
 	var links []string
+	// This function will decide what to do with an html tag
 	visitNode := func(n *html.Node) {
-		if n.Type == html.ElementNode && n.Data == "a" {
-			for _, a := range n.Attr {
-				if a.Key != "href" {
-					continue
-				}
-				if len(a.Val) == 0 {
-					continue // Ignoring empty links
-				}
-				// Check for duplicate links (in the current URL)
-				if linkExists(a.Val, links) {
-					logLine(id, "ignoring duplicate link %s", a.Val)
-					continue
-				}
-				links = append(links, a.Val)
-			}
+		// We only want <a> html tags
+		if n.Type == html.ElementNode && n.Data != "a" {
+			return
 		}
+		// Let's inspect all attributes in the tag
+		for _, a := range n.Attr {
+			// Check if attributes is "href"
+			if a.Key != "href" {
+				continue
+			}
+			// Ignoring empty links
+			if len(a.Val) == 0 {
+				continue
+			}
+			// Ignoring duplicate links
+			if linkExists(a.Val, links) {
+				logLine(id, "ignoring duplicate link %s", a.Val)
+				continue
+			}
+			// Add links to the url links array
+			links = append(links, a.Val)
+		}
+
 	}
+	// Parse document and pass our tag parser
 	forEachNode(body, visitNode, nil)
 	for link := range links {
 		// Add link (even if malformed) to the link list
 		logLine(id, "found link %s", links[link])
 		urls.URLS[id].Links = append(urls.URLS[id].Links, links[link])
-		// Now we have to determine if the link is a valid URL and starts like the initial one
+		// If it's a valid url, add it to the database and start a walk process
 		if validURL(id, links[link]) {
 			logLine(id, "adding url %s for being processed", links[link])
 			urls.URLS = append(urls.URLS, *newUrl(links[link]))
 			go fetchUrl(len(urls.URLS)-1, links[link])
 		}
 	}
+	// Document parsed and all link extracted and evaluated. Mark as completed.
 	completeUrl(id, 0)
 }
 
+// Recursive function for extract html tags
 func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
 	if pre != nil {
 		pre(n)
@@ -180,42 +206,45 @@ func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
 	}
 }
 
+// See if all urls in the database are completed
 func allDone() bool {
-	for u := range urls.URLS {
-		if !urls.URLS[u].Completed {
+	for _, u := range urls.URLS {
+		if !u.Completed {
 			return false
 		}
 	}
-	return true // All URLs have been processed, beer time!
+	// All URLs have been processed, beer time!
+	return true
 }
 
+// Main function
 func main() {
 	// Command-Line options
-	verbose = flag.Bool("verbose", false, "Enable verbose mode")
-	resume = flag.Bool("resume", false, "Resume last execution")
-	initialUrl = flag.String("url", "", "Initial URL to fetch")
-	outputFile = flag.String("output", "output.json", "Output filename")
+	flag.BoolVar(&verbose, "verbose", false, "Enable verbose mode")
+	flag.BoolVar(&resume, "resume", false, "Resume last execution")
+	flag.StringVar(&initialUrl, "url", "", "Initial URL to fetch")
+	flag.StringVar(&outputFile, "output", "output.json", "Output filename")
 
 	flag.Parse()
 
-	if strings.EqualFold(*outputFile, workingFile) {
+	if strings.EqualFold(outputFile, workingFile) {
 		log.Fatal("Please use other output filename, since ", workingFile, " is reserved")
 	}
 
-	if len(*initialUrl) == 0 && !*resume {
+	if len(initialUrl) == 0 && !resume {
 		log.Fatal("No initial url provided, only allowed when resuming")
 	}
 
-	if *verbose {
-		log.Println("Resume:", *resume)
-		log.Println("InitialURL:", *initialUrl)
-		log.Println("OutputFile:", *outputFile)
+	if verbose {
+		log.Println("Resume:", resume)
+		log.Println("InitialURL:", initialUrl)
+		log.Println("OutputFile:", outputFile)
 	}
 
 	// Check if there is previous data
 	_, err := os.Stat(workingFile)
 	if !errors.Is(err, os.ErrNotExist) {
-		if !*resume {
+		if !resume {
 			log.Println("FATAL: Working file found!")
 			log.Fatal("Use -resume flag for resume the execution, or remove it and execute again!")
 		}
@@ -224,16 +253,16 @@ func main() {
 		if err != nil {
 			log.Fatal("Error parsing", workingFile) // stop program
 		}
-		if *verbose {
+		if verbose {
 			log.Println("Working file loaded")
 		}
 		if len(urls.URLS) == 0 {
 			log.Fatal("Empty or malformed working.json, please check or delete it", workingFile) // stop program
 		}
-		*initialUrl = urls.URLS[0].URL
+		initialUrl = urls.URLS[0].URL
 	} else {
 		// First URL insertion
-		first := newUrl(*initialUrl)
+		first := newUrl(initialUrl)
 		urls.URLS = append(urls.URLS, *first)
 	}
 
@@ -252,11 +281,11 @@ func main() {
 	// Save to disk
 	if len(urls.URLS) > 0 {
 		file, _ := json.MarshalIndent(urls, "", " ")
-		_ = ioutil.WriteFile(*outputFile, file, 0644)
+		_ = ioutil.WriteFile(outputFile, file, 0644)
 		_ = os.Remove(workingFile)
-		if *verbose {
+		if verbose {
 			log.Println("All done!")
-			log.Printf("File %s saved with %d URLS.\n", *outputFile, len(urls.URLS))
+			log.Printf("File %s saved with %d URLS.\n", outputFile, len(urls.URLS))
 		}
 	}
 }
